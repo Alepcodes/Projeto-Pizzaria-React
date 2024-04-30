4abcb0e862b4b6d9ed9ccbf2b3adb65bc37988a4
Alexandre Paulino De Paula <alexandrepaula49@gmail.com>
2024-04-30 12:02:56 -0300

    criando os cards

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,13 @@ const Footer = () => {
     return (
         <>
             <h1>Footer</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.0682227196994!2d-38.571871425025876!3d-3.7953070961785365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74c2a874fc031%3A0xb26e696f6524c020!2sDona%20Pizza%20Delivery!5e0!3m2!1spt-BR!2sbr!4v1714483394025!5m2!1spt-BR!2sbr" 
+            width="100%" 
+            height="300" 
+            style={{border: 0}} 
+            allowFullScreeneen="" 
+            loading="lazy" 
+            referrerPolicypolicy="no-referrer-when-downgrade"></iframe>
         </>
     );
 }
